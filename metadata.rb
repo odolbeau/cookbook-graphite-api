b7589839461d0e9695c87c359e026882880fd500
Olivier Dolbeau <contact@odolbeau.fr>
2014-08-26 14:45:02 +0200

    Bump to version 1.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'odolbeau@gmail.com'
 license          'MIT'
 description      'Installs graphite-api'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.2.2'
 
 recipe 'graphite-api', 'Installs graphite-api.'
 
